cc3289319933c9316d2f15d179ce24749b5b6e3a
James Miller <james.rex.miller.4@gmail.com>
2019-08-18 19:04:23 -0600

    Revised according to js style guide

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ var headchange = document.getElementById('somedogs');
 var btn = document.getElementById('namebtn');
 
 
-btn.addEventListener('click', function(){
+btn.addEventListener('click', function() {
     var fromInput = bar.value;
     headchange.innerHTML = fromInput;
     bar.value = '';
@@ -12,20 +12,20 @@ btn.addEventListener('click', function(){
 var para1 = document.querySelector('.p1');
 var btnclm1 = document.querySelector('.howbtn');
 
-btnclm1.addEventListener('click', function(){
-  para1.classList.toggle('disappear');
+btnclm1.addEventListener('click', function() {
+    para1.classList.toggle('disappear');
 });
 
 var para2 = document.querySelector('.p2');
 var btnclm2 = document.querySelector('.whatbtn');
 
-btnclm2.addEventListener('click', function(){
-  para2.classList.toggle('disappear');
+btnclm2.addEventListener('click', function() {
+    para2.classList.toggle('disappear');
 });
 
 var para3 = document.querySelector('.p3');
 var btnclm3 = document.querySelector('.factbtn');
 
-btnclm3.addEventListener('click', function(){
-  para3.classList.toggle('disappear');
+btnclm3.addEventListener('click', function() {
+    para3.classList.toggle('disappear');
 });
