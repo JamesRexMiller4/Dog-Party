05eb668c190591d1b51707e2f95d0a0a61fc79f0
James Miller <james.rex.miller.4@gmail.com>
2019-09-27 01:47:28 -0600

    Refactor code with JQuery

--- main.js
+++ main.js
@@ -1,31 +1,20 @@
-var bar = document.getElementById('barinput');
-var headchange = document.getElementById('somedogs');
-var btn = document.getElementById('namebtn');
+var bar = $('#barinput');
+var headchange = $('#somedogs');
+var btn = $('#namebtn');
+var para1 = $('.p1');
+var btnclm1 = $('.howbtn');
+var para2 = $('.p2');
+var btnclm2 = $('.whatbtn');
+var para3 = $('.p3');
+var btnclm3 = $('.factbtn');
 
-
-btn.addEventListener('click', function() {
+// Event Listeners
+btn.on('click', () => {
     var fromInput = bar.value;
     headchange.innerHTML = fromInput;
     bar.value = '';
 });
 
-var para1 = document.querySelector('.p1');
-var btnclm1 = document.querySelector('.howbtn');
-
-btnclm1.addEventListener('click', function() {
-    para1.classList.toggle('disappear');
-});
-
-var para2 = document.querySelector('.p2');
-var btnclm2 = document.querySelector('.whatbtn');
-
-btnclm2.addEventListener('click', function() {
-    para2.classList.toggle('disappear');
-});
-
-var para3 = document.querySelector('.p3');
-var btnclm3 = document.querySelector('.factbtn');
-
-btnclm3.addEventListener('click', function() {
-    para3.classList.toggle('disappear');
-});
+btnclm1.on('click', () => para1.attr('style') !== "display: none;"?  para1.slideUp() : para1.slideDown());
+btnclm2.on('click', () => para2.attr('style') !== "display: none;"? para2.slideUp() : para2.slideDown());
+btnclm3.on('click', () => para3.attr('style') !== "display: none;"? para3.slideUp() : para3.slideDown());
